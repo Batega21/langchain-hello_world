ddce1f74e8bb5eae103677a63f414fe0339b5f1c
Gabriel Gomez <batega21@gmail.com>
2025-11-03 13:35:25 -0300

    langchain Hello world - first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+
+def main():
+    print("Hello from hello-world!")
+
+
+if __name__ == "__main__":
+    main()
