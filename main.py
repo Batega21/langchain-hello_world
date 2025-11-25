7e9a0724762fb8fc327798c69f5ded8f231778eb
Gabriel E Gomez <batega21@gmail.com>
2025-11-25 11:55:05 -0300

    Tavily langchain tavily search tool

--- main.py
+++ main.py
@@ -3,26 +3,12 @@ from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
-from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 
 load_dotenv()
 
-tavily = TavilyClient()
-
-@tool
-def search_tool(query: str) -> str:
-    """
-    Tool that searches the web
-    Args:
-        query (str): The search query
-    Returns:
-        str: The search results
-    """
-    print(f"Searching for: {query}")
-    return tavily.search(query=query)
-
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite",temperature=0)
-tools = [search_tool]
+tools = [TavilySearch()]
 agent = create_agent(model=llm, tools=tools)
 
 def main():
