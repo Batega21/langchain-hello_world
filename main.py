899e751e478884ef1240162b9195ebf0ec06eedc
Gabriel E Gomez <batega21@gmail.com>
2025-12-01 09:46:18 -0300

    react agents

--- main.py
+++ main.py
@@ -1,40 +1,32 @@
 from dotenv import load_dotenv
 
-from typing import List
-from pydantic import BaseModel, Field
-
-from langchain.agents import create_agent
-from langchain.tools import tool
-from langchain_core.messages import HumanMessage
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_tavily import TavilySearch
-
-class Source(BaseModel):
-    """Schema for a source used by the agent"""
-    url: str = Field(description="The URL of the source")
-
-
-class AgentResponse(BaseModel):
-    """Schema for agent response with answer and sources"""
-    answer: str = Field(description="Thr agent's answer to the query")
-    sources: List[Source] = Field(
-        default_factory=list, description="List of sources used to generate the answer"
-    )
-
 load_dotenv()
 
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite",temperature=0)
+from langchain_classic import hub
+from langchain_classic.agents import AgentExecutor
+from langchain_classic.agents.react.agent import create_react_agent
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
+llm = ChatOpenAI(model="gpt-4")
+react_prompt = hub.pull("hwchase17/react")
+agent = create_react_agent(
+    llm=llm,
+    tools=tools,
+    prompt=react_prompt,
+)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+chain = agent_executor
 
 def main():
-    result = agent.invoke({
-        "messages": [
-            HumanMessage(content="search for 3 job postings for a Digital Project Manager in the Argentina area on linkedin and list their details")
-        ]
-    })
-    print("Agent Result:", result)
-    
+    result = chain.invoke(
+        input={
+            "input": "search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details",
+        }
+    )
+    print(result)
+
 
 if __name__ == "__main__":
     main()
