d8629842f898f465a400103a3c061cde5887ca3d
Gabriel E Gomez <batega21@gmail.com>
2025-12-02 09:44:04 -0300

    adding output parser runnables

--- main.py
+++ main.py
@@ -14,21 +14,38 @@ from langchain_tavily import TavilySearch
 from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
 from schemas import AgentResponse
 
+# Tools
 tools = [TavilySearch()]
+
+# LLM
 llm = GoogleGenerativeAI(model="gemini-2.0-flash-exp")
+
+# Output Parser
 output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+
+# React Prompt
 react_prompt_with_format_instructions = PromptTemplate(
     template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
     input_variables=["input", "agent_scratchpad", "tool_names"],
 ).partial(format_instructions=output_parser.get_format_instructions())
+
+# Agent Executor
 agent = create_react_agent(
     llm=llm,
     tools=tools,
     prompt=react_prompt_with_format_instructions,
 )
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-chain = agent_executor
 
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+# Runnable function to extract output
+extract_output = RunnableLambda(lambda x: x["output"])
+
+# Runnable function to parse output
+parse_output = RunnableLambda(lambda x: output_parser.parse(x))
+
+#  LangChain Chain
+chain = agent_executor | extract_output | parse_output
 
 def main():
     result = chain.invoke(
