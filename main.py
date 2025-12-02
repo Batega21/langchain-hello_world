f8f77020a8c4e7f6c0b4aeff6f02f6e12a1dae8d
Gabriel E Gomez <batega21@gmail.com>
2025-12-02 09:01:51 -0300

    adding output parser

--- main.py
+++ main.py
@@ -5,19 +5,30 @@ load_dotenv()
 from langchain_classic import hub
 from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents.react.agent import create_react_agent
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 from langchain_google_genai import GoogleGenerativeAI
 from langchain_tavily import TavilySearch
 
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
+
 tools = [TavilySearch()]
 llm = GoogleGenerativeAI(model="gemini-2.0-flash-exp")
-react_prompt = hub.pull("hwchase17/react")
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input", "agent_scratchpad", "tool_names"],
+).partial(format_instructions=output_parser.get_format_instructions())
 agent = create_react_agent(
     llm=llm,
     tools=tools,
-    prompt=react_prompt,
+    prompt=react_prompt_with_format_instructions,
 )
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 chain = agent_executor
+
 
 def main():
     result = chain.invoke(
@@ -26,6 +37,14 @@ def main():
         }
     )
     print(result)
+
+    # Debug Console
+      # type(result) <class 'dict'>
+      # type(result["output"]) <class 'str'>
+      # output_parser.parse(result["output"]) <class 'AgentResponse'>
+      # type(output_parser.parse(result["output"])) <class 'schemas.AgentResponse'>
+      # output_parser.parse(result["output"]).answer === AI answer
+      # output_parser.parse(result["output"]).sources === job posting sources
 
 
 if __name__ == "__main__":
---
+++ schemas.py
@@ -0,0 +1,18 @@
+from typing import List
+
+from pydantic import BaseModel, Field
+
+
+class Source(BaseModel):
+    """Source of the job posting"""
+
+    url: str = Field(description="URL of the job posting")
+
+
+class AgentResponse(BaseModel):
+    """Schema for the agent response"""
+
+    answer: str = Field(description="Answer to the question")
+    sources: List[Source] = Field(
+        description_factory=list, description="Sources of the answer"
+    )
